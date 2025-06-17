60c44f41b5bfb9e8e08492cff5290da07a2b50a9
EmoGeorgiev <unstabl3@Emo.localdomain>
2025-06-18 00:51:24 +0300

    Added SecurityUser and an initial version of JpaUserDetailsService

---
+++ Reddit-backend/src/main/java/com/example/reddit/user/JpaUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.reddit.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public JpaUserDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
---
+++ Reddit-backend/src/main/java/com/example/reddit/user/SecurityUser.java
@@ -0,0 +1,31 @@
+package com.example.reddit.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+    private final RedditUser user;
+
+    public SecurityUser(RedditUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+}
