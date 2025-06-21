0cb0a0fc2ccc5385eb760bb720ff81a8b49be603
EmoGeorgiev <emogeorgiev788@gmail.com>
2025-06-21 22:25:22 +0300

    Implemented the method in the VoteCointroller class

--- Reddit-backend/src/main/java/com/reddit/vote/VoteController.java
+++ Reddit-backend/src/main/java/com/reddit/vote/VoteController.java
@@ -1,7 +1,24 @@
 package com.reddit.vote;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VoteController {
+    private final VoteService voteService;
+
+    public VoteController(VoteService voteService) {
+        this.voteService = voteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> voteForContent(@RequestBody @Valid VoteDto voteDto) {
+        voteService.voteForContent(voteDto);
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 }
