76d30ba2d0258dfeba7728b0f8fbbf7d68a126df
EmoGeorgiev <emogeorgiev788@gmail.com>
2025-06-25 17:41:05 +0300

    Removed the toggleContent method from ContentService. Removed the findSavedContentByUserId method from ContentRepository

--- Reddit-backend/src/main/java/com/reddit/content/ContentRepository.java
+++ Reddit-backend/src/main/java/com/reddit/content/ContentRepository.java
@@ -1,12 +1,6 @@
 package com.reddit.content;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
-    @Query("SELECT c FROM Content c JOIN FETCH c.savedBy u WHERE u.id = :userId")
-    Page<Content> findSavedContentByUserId(@Param("userId") Long userId, Pageable pageable);
 }
--- Reddit-backend/src/main/java/com/reddit/content/ContentService.java
+++ Reddit-backend/src/main/java/com/reddit/content/ContentService.java
@@ -1,14 +1,8 @@
 package com.reddit.content;
 
-import com.reddit.content.dto.ContentDto;
 import com.reddit.exception.content.ContentNotFoundException;
-import com.reddit.user.RedditUser;
-import com.reddit.user.UserService;
 import com.reddit.util.ErrorMessages;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ContentService {
@@ -22,18 +16,5 @@ public class ContentService {
         return contentRepository
                 .findById(id)
                 .orElseThrow(() -> new ContentNotFoundException(ErrorMessages.CONTENT_NOT_FOUND));
-    }
-
-    public void toggleSaveContent(Long contentId, Long userId) {
-        Content content = getContentEntity(contentId);
-        RedditUser user = userService.getUserEntity(userId);
-
-        if (!user.getSaved().contains(content)) {
-            user.getSaved().add(content);
-            content.getSavedBy().add(user);
-        } else {
-            user.getSaved().remove(content);
-            content.getSavedBy().remove(user);
-        }
     }
 }
