55094fe46d40c03cbcd7d0dd3367d46d41b7e3bb
EmoGeorgiev <emogeorgiev788@gmail.com>
2025-07-02 16:57:05 +0300

    Added the AuthContext, AuthProvider and ProtectedRoute components

---
+++ Reddit-frontend/src/components/Authentication/AuthProvider.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import { AuthContext } from './AuthContext'
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        loadUser()
+    }, [])
+
+    const login = (data) => {
+        const localStorageUser = {
+            'username': data.username,
+            'id': data.id 
+        }
+
+        window.localStorage.setItem('loggedUser', JSON.stringify(localStorageUser))
+        window.localStorage.setItem('token', JSON.stringify(data.token))
+
+        setUser(localStorageUser)
+        setIsAuthenticated(true)
+    }
+
+    const logout = () => {
+        localStorage.removeItem('loggedUser')
+        localStorage.removeItem('token')
+
+        setUser(null)
+        setIsAuthenticated(false)
+    }
+
+    const loadUser = () => {
+        const localStorageUser = JSON.parse(localStorage.getItem('loggedUser'))
+
+        if (localStorageUser != null) {
+            setUser(localStorageUser)
+            setIsAuthenticated(true)
+        }
+        setIsLoading(false)
+    }
+    
+    return (
+        <AuthContext.Provider value={{ login, logout, user, isAuthenticated, isLoading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
